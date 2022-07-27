5f3a1716d9dd76b90e7b83a896ae5f739ff3ed01
Almog <aalmogiiii@gmail.com>
2022-07-27 11:35:31 +0300

    fixed registration details

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a style={{fontWeight: 'bold'}} className="nav-link active" aria-current="page" href="#">Home Page</a>
+                        <a style={{fontWeight: 'bold'}} className="nav-link active" aria-current="page" href="#">MamiMevi</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">Log in</a>
