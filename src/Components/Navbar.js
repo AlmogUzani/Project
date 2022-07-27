3bdeb876b1b82e207781b79eb6bda5fd8fa216c7
Almog <aalmogiiii@gmail.com>
2022-07-27 15:29:05 +0300

    edit menu icon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
+                    <img src="/../images/icons/menu.png" alt="menu" className="iconL" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
