ee4e6dc5c341ddd791399b45752a17c577f4d7bf
Almog <aalmogiiii@gmail.com>
2022-07-28 11:28:52 +0300

    edited productpage with state and effect

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -1,12 +1,29 @@
-import {Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { useEffect, useState } from 'react';
+import { getProductById } from '../DAL/api'; 
+import { getCategoriesById } from '../DAL/api'
+
 
 
 function ProductDetails(props) {
+    const [product, setProduct] = useState([])
+    const [currentCategory, setCurrentCategory] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function getData(id) {
+            setProduct(await getProductById(id))
+            setCurrentCategory(await getCategoriesById(id))
+            setLoading(false)
+        }
+        getData(props.id)
+    },[])
         return (
             <div>
                 <Navbar></Navbar>
+                {loading? 'Loading' :
                 <main>
                     <Container style={{display: 'flex'}}>
                         <Row>
@@ -16,13 +33,13 @@ function ProductDetails(props) {
 
                                     <div className="carousel-inner">
                                     <div className="carousel-item active">
-                                        <img src="/../images/itemsByCategories/vegtables/cucumber1.jpg" className="d-block w-100" alt="Cucumber" />
+                                        <img src={product.image1} className="d-block w-100" alt={product.name} />
                                     </div>
                                     <div className="carousel-item">
-                                        <img src="/../images/itemsByCategories/vegtables/cucumber2.jpg" className="d-block w-100" alt="Cucumber" />
+                                        <img src={product.image2} className="d-block w-100" alt={product.name} />
                                     </div>
                                     <div className="carousel-item">
-                                        <img src="/../images/itemsByCategories/vegtables/cucumber3.jpg" className="d-block w-100" alt="Cucumber" />
+                                        <img src={product.image3} className="d-block w-100" alt={product.name} />
                                     </div>
                                     </div>
                                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -40,18 +57,18 @@ function ProductDetails(props) {
                                 <div className="card text-bg-light" id='fullCard'>
                                     <div className="card" id='fullCard'>
                                         <div className="card-body">
-                                        <h5 className="card-title">Cucumber</h5>
+                                        <h5 className="card-title">{product.name}</h5>
                                         <p className="card-text">
-                                            Green & Fresh cucumber straight from the greenhouse.
+                                            {product.description}
                                         </p>
                                         </div>
                                         <ul className="list-group list-group-flush">
                                         <li className="list-group-item">
                                             <ul>
-                                            <li>Size: L</li>
-                                            <li>Shape: long</li>
-                                            <li>Color: green</li>
-                                            <li>Category: vegtables</li>
+                                            <li>Size: {product.size}</li>
+                                            <li>Shape: {product.shape}</li>
+                                            <li>Color: {product.color}</li>
+                                            <li>Category: {currentCategory.name}</li>
                                             </ul>
                                         </li>
                                         </ul>
@@ -60,7 +77,7 @@ function ProductDetails(props) {
                                                 <button type="button" className="btn btn-primary btn-sm">+</button>
                                                 <span style={{alignItems: 'center', margin: '0 10px'}}>X</span>
                                                 <button type="button" className="btn btn-secondary btn-sm" style={{marginRight: '10px'}}>-</button>
-                                                Quantity: (154 pieces/Kg in stock)
+                                                Quantity: ({product.unit_in_stock} pieces/Kg in stock)
                                             </p>
                                             <button type="button" className="btn btn-secondary">Add to cart <img src="https://static.vecteezy.com/system/resources/previews/004/999/463/original/shopping-cart-icon-illustration-free-vector.jpg" alt="cart" style={{height: '50px', width: '50px'}} /></button>
                                         </div>
@@ -70,6 +87,7 @@ function ProductDetails(props) {
                         </Row>
                     </Container>
                 </main>
+                }
                 <Footer></Footer>
             </div>
 )}
